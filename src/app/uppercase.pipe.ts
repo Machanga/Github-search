a2dc899947914f48a246566aef8edad1f3bbbd20
Machanga <florenceshiru765@gmail.com>
2019-06-09 19:36:53 +0300

    Installing bootstrap to our file fro styling.

--- src/app/uppercase.pipe.ts
+++ src/app/uppercase.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UppercasePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string): any {
+    return value.toUpperCase();
   }
 
 }
