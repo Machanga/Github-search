bf7da8dc925e3744bb422815ce39664b6f6a2019
Machanga <florenceshiru765@gmail.com>
2019-06-09 19:32:09 +0300

    Define the elem property that we assign the type ElementRef which we then inport together with the HOstLIstener decorator that will listen for a mouse move and highlight the text.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
