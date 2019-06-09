d4c2061d2b9e8acf1da3e106eb364f39d6c1f5d5
Machanga <florenceshiru765@gmail.com>
2019-06-09 20:01:26 +0300

    Inject the ProfileSErvice into the profile component constructor and assign it to the property profileService.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {ProfileService} from '../profiles/profile.service';
+import {User} from '../user';
+import {Repo} from '../repo';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  providers:[ProfileService],
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user:User;
+  repo: Repo;
+  reposArray: any[];
+  public username: string;
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit() {
   }
--- src/app/profiles/profile.service.ts
+++ src/app/profiles/profile.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { User } from '../user';
+import { Repo } from '../repo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
+  user:User;
+  repo:Repo;
+  reposArray:any;
+  private username:string;
   constructor() { }
 }
