21e855697f9535bb2f2ace06d8a7942e1ee1f27e
Machanga <florenceshiru765@gmail.com>
2019-06-09 19:18:02 +0300

    DEfine the properties for the user class.

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,3 @@
 export class User {
+    constructor(public name:string, public login:string, public location:string, public avatar_url:string,public bio:string, public blog:string, public email:string, public followers:number, public following:number, public public_gists:number, public public_repos:number, public html_url:string){}
 }
